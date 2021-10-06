ffd7e0b32c6bf343205aa5addedf0f6b9400f5e8
radoslawglowacki <radglo1995@gmail.com>
2021-10-06 23:45:35 +0200

    Add basic functionalities like selecting word, display stats and game difficult

--- hangman.sh
+++ hangman.sh
@@ -1,8 +1,82 @@
 #! /bin/bash
 
+Words=('Poland' 'Germany' 'England' 'France' 'Spain')
 
+function selectWord(){
+size=${#Words[@]}
+index=$(($RANDOM % $size))
+echo ${Words[$index]}
+}
+
+function preparingDashes(){
+len=`expr length $1`
+local Dashes=()
+for ((i=0; i < len; i++)); do
+	Dashes+=('_')
+done 
+echo ${Dashes[@]}
+}
+
+function gameDifficult(){
+select level in EASY MEDIUM HARD
+do
+	case $level in
+	EASY)
+		echo 9
+		return;;
+	MEDIUM)
+		echo 6
+		return;;
+	HARD)
+		echo 3
+		return;;
+	*)
+		echo "ERROR! Please select between 1..3";;
+	esac
+done
+}
+
+function mainMenu(){
+
+echo "
+Welcome in simple Hangman Game written by Radoslaw Glowacki in Bash Script
+
+                  Select Difficult Level and have fun!
+"
+}
+
+
+function displayStats(){
+clear
+echo "
+Word to guess: $1
+
+Used letters: $3
+
+Chances: $2
+"
+}
+
+# TODO
+# function to check result
+# main game engine
 
 
 function game(){
+local SelectedWord=$(selectWord)
+local Dashes=$(preparingDashes $SelectedWord)
+local UsedLetters=()
+local Chances=$(gameDifficult)
+
+displayStats "${Dashes[@]}" $UsedLetters $Chances
 
 }
+
+
+function main(){
+mainMenu
+game
+
+}
+
+main
